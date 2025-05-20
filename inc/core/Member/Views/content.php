5a430adfe515f17e1b4360880bdc1df7a416c292
piyush barve <piyushbavre06@gmail.com>
2025-05-20 11:37:48 +0530

    lkkdsjfs

--- inc/core/Member/Views/content.php
+++ inc/core/Member/Views/content.php
@@ -4,20 +4,23 @@
         <div class="card-title d-flex justify-content-between align-items-center">
             <h4><?= $heading ?></h4>
         </div>
-        <form id="<?= $formid ?>">
-            <?php if ($editit) { ?>
-                <input type="hidden" name="edit" value="<?= $editit ?>">
-            <?php } ?>
+            
             <div class="tab-pane fade active show">
                 <div class="card-body">
-                    <?php _ec( $this->include($contentfilename), false )?>
+                    <form id="inputform1">
+                        <?php if ($editit) { ?>
+                            <input type="hidden" name="edit" value="<?= $editit ?>">
+                        <?php } ?>
+                        <?php _ec( $this->include($contentfilename), false )?>
+                    </form>
+                    <form id="inputform2">
+                        <?php _ec( $this->include('Core\Member\Views\contentperfectpage'), false )?>
+                    </form>
                     <div class="row mt-3">
                         <div class="form-buttons mt-3 ">
-                            <!-- Save and Close Button -->
-                            <button type="submit" class="btn btn-primary" id="savebtn">
+                            <a type="button" class="btn btn-primary" id="savebtn">
                                 <i class="fas fa-save me-2"></i> Save & Close
-                            </button>
-                            <!-- Cancel Button -->
+                            </a>
                             <a class="btn btn-danger" id="cancel-btn" href="javascript:void(0)" onclick="history.back()" role="button">
                                 <i class="fas fa-times me-2"></i> Cancel
                             </a>
@@ -25,9 +28,17 @@
                     </div>
                 </div>
             </div>
-        </form>
     </div>
 </div>
 <?php echo $this->section('script') ?>
-    <?php echo view('common_script/formsubmit', ['formid' => $formid, 'submitbtn' => 'savebtn', 'formurl' => $formroute]); ?>
+    <script>
+        $(document).ready(function(){
+            $('#savebtn').click(function(){
+                $('#inputform1').submit();
+                $('#inputform2').submit();
+            })
+        })
+    </script>
+    <?php echo view('common_script/formsubmit', ['formid' => 'inputform1', 'submitbtn' => 'savebtn', 'formurl' => 'member/datasetup']); ?>
+    <?php echo view('common_script/formsubmit', ['formid' => 'inputform2', 'submitbtn' => 'savebtn', 'formurl' => 'perfect/datasetup']); ?>
 <?php echo $this->endSection() ?>
